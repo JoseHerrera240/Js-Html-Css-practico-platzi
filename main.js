6be6da97a7f8fc86ebfa6f9caabc84344248219e
Jose <hjose0650@gmail.com>
2022-10-24 08:39:04 -0500

    change folders to platzi

--- main.js
+++
@@ -1,8 +0,0 @@
-const menuEmai = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-
-menuEmai.addEventListener('click', toggleDesktopMenu);
-
-function toggleDesktopMenu() {
-    desktopMenu.classList.remove("inactive")
-}
